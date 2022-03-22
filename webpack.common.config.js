75efcf0a75414735f3fcc7b72d01b971bc33034a
Aaron Granick <aaron.granick@okta.com>
2022-03-22 11:20:12 -0700

    use preset-typescript instead of ts-loader

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -3,16 +3,26 @@ var webpack = require('webpack');
 var SDK_VERSION = require('./package.json').version;
 
 var babelOptions = {
-  presets: ['@babel/preset-env'],
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-transform-runtime'
+  configFile: false, // do not load from babel.config.js
+  babelrc: false, // do not load from .babelrc
+  presets: [
+    '@babel/preset-typescript'
   ],
+  plugins: [],
   sourceType: 'unambiguous',
   // the banners should only be added at the end of the build process
   // ignore all comments during transpiling
   shouldPrintComment: () => false 
 };
+
+// Keep source files untransformed for local development
+if (process.env.NODE_ENV !== 'development') {
+  babelOptions.presets.unshift('@babel/preset-env'); // must run after preset-typescript
+  babelOptions.plugins = babelOptions.plugins.concat([
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-runtime'
+  ]);
+}
 
 var babelExclude = /node_modules\/(?!p-cancelable)/;
 
@@ -20,23 +30,10 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.[jt]s$/,
         exclude: babelExclude,
         loader: 'babel-loader',
         options: babelOptions
-      },
-      {
-        test: /\.ts$/,
-        exclude: babelExclude,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: babelOptions
-          },
-          {
-            loader: 'ts-loader'
-          }
-        ]
       }
     ]
   },
